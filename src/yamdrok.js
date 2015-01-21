e5e6f62ba1bfce02cd658592840d3ac1b9bcedfb
takumi4ichi <takumi4ichi@gmail.com>
2015-01-21 11:29:10 +0800

    完善基本结构

--- src/yamdrok.js
+++ src/yamdrok.js
@@ -21,7 +21,64 @@ var yamdrok = (function(){
     var viewport_sym = jcon.string('@viewport');
     var keyframes_sym = jcon.regex(/@(?:-[a-z]+-)?keyframes/);
 
-    var stylesheet = jcon.or();
+
+    var stylesheet = jcon.seq(charset.possible(), scd_list.possible(), import_list.possible(), namespace_list.possible(), entity_list.possible());
+
+    var space_list = jcon.lazy(function(){
+        return jcon.seq(space_list.possible(), S);
+    });
+
+    var scd_list = jcon.lazy(function(){
+        return jcon.seq(scd_list.possible(), jcon.or(S, CDO, CDC));
+    });
+    
+    var namespace_list = jcon.lazy(function(){
+        return jcon.seq(namespace_list.possible(), namespace, scd_list.possible());
+    });
+
+    var import_list = jcon.lazy(function(){
+        return jcon.seq(import_list.possible(), import, scd_list.possible());
+    });
+
+    var entity_list = jcon.lazy(function(){
+        return jcon.seq(entity_list.possible(), jcon.or(ruleset, media, page, font_face, viewport, keyframes_rule), scd_list.possible());
+    });
+
+    var keyframes_rule = jcon.seq(
+        keyframes_sym,
+        space_list,
+        ident,
+        space_list.possible(),
+        jcon.string('{'),
+        space_list.possible(),
+        keyframes_blocks,
+        jcon.string('}'),
+        space_list.possible()
+    );
+
+    var kyeframes_selector = jcon.lazy(function(){
+        return jcon.seq( jcon.or(from_sym, to_sym, percentage), space_list.possible() );
+    });
+
+    var keyframes_block = jcon.lazy(function(){
+        return jcon.seq(keyframes_blocks.possible(), keyframe_selector, jcon.string('{'), space_list.possible(), declaration_list, jcon.string('}'), space_list.possible());
+    });
+
+    var charset = jcon.seq(charset_sym, space_list.possible(), string, space_list.possible(), jcon.string(';'));
+
+    var medium_list = jcon.lazy(function(){
+        return jcon.or(
+            medium,
+            jcon.seq(medium_list.possible(), jcon.string(','), space_list.possible(), medium)
+        );
+    });
+
+    var import = jcon.or(
+        jcon.seq(import_sym, space_list.possible(), uri, space_list.possible(), medium_list.possible(), jcon.string(';'), space_list.possible()),
+        jcon.seq(import_sym, space_list.possible(), string, space_list.possible(), medium_list.possible(), jcon.string(';'), space_list.possible())
+    );
+
+
 
     return stylesheet;
 
